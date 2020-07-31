988c8d45afa8e91ad55f96129ed8a685b82ba9a8
Aimee Cao <v-liucao@microsoft.com>
2020-07-31 03:00:41 +0000

    happy

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,7 +8,6 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
